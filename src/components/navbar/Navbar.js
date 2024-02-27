7029dc5ce53769af02cd48a639ce91bc4487ade0
Sandeep <sandeep243123@gmail.com>
2024-02-27 22:26:22 +0530

    navbar updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,8 +2,18 @@ import React from "react";
 import { Link } from 'react-router-dom'
 import './style.css';
 import img1 from './logo-removebg-preview.png'
+import userImg from '../popup components/view contribution/user.png'
+import logoutImg from '../assets/switch.png'
+import signupImg from '../assets/add-friend.png'
+import loginImg from '../assets/login.png'
+import contactImg from '../assets/customer-service.png'
+import aboutImg from '../assets/about-us.png'
+import exploreImg from '../assets/explore.png'
+import contributeImg from '../assets/jigsaw.png'
 
+import { useState } from "react";
 export default function Navbar() {
+  const [condition, setCondition] = useState(true)
   return (
     <div>
       <div class="nav">
@@ -14,19 +24,21 @@ export default function Navbar() {
           </div>
         </div>
         <div class="nav-btn">
-          <label for="nav-check">
-            <span></span>
-            <span></span>
-            <span></span>
+          <label className="userImg" htmlFor="nav-check">
+            <img src={userImg} alt="" style={{ width: "100%", height: '100%' }} />
           </label>
         </div>
         <div class="nav-links">
 
-          <Link to={'/explore'}>Explore</Link>
-          <Link to={'/about'}>About us</Link>
-          <Link to={'/contact'}>Contact us</Link>
-          <Link to={'/login'}>Login</Link>
-          <Link to={'/signup'}>Signup</Link>
+          <Link to={'/explore'}><img src={exploreImg} className="icon-img"></img>Explore</Link>
+          <Link to={'/about'}><img src={aboutImg} className="icon-img"></img>About us</Link>
+          <Link to={'/contact'}><img src={contactImg} className="icon-img"></img>Contact us</Link>
+          {condition ? <Link to={'/login'}><img src={loginImg} className="icon-img"></img>Login</Link> : null}
+          {condition ? <Link to={'/signup'}><img src={signupImg} className="icon-img"></img>Signup</Link> : null}
+          <Link><img src={contributeImg} className="icon-img" alt=""></img>Contribute</Link>
+
+          <Link to={'/contact'}><img src={logoutImg} className="icon-img" alt=""></img>Sign out</Link>
+
         </div>
       </div>
     </div>
