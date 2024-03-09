680ed570aa6e190d8e367cfbc3a92a8284b065cf
HARIKESH KUSHWAHA <75518548+HarikeshKushwaha18@users.noreply.github.com>
2024-03-09 21:41:52 +0530

    Add files via upload

---
+++ src/components/about us/About.js
@@ -0,0 +1,164 @@
+import React from "react";
+import style from "../about us/About.module.css";
+import aboutus1 from "../assets/aboutus1.png";
+import aboutus2 from "../assets/aboutus2.png";
+export default function About() {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.heading}>
+        <p>About </p> <p>eFloraOfIndia</p>
+      </div>
+      <div className={style.container}>
+        <div className={style.parentLeft}>
+          <div className={style.contentLeft}>
+            <div className={style.head}>1. Outstanding Contributers</div>
+            <div className={style.mainContent}>
+              Outstanding Contributors: . Name (Main area of expertise on Flora
+              of India) Sh. J.M. Garg (efloraofindia website) Sh. Saroj Kumar
+              Kasaju (Flora Documentation of Nepal) Dr. Gurcharan Singh (Western
+              Himalayas) Dr Pankaj Kumar (Orchidaceae) Sh. Dinesh Valke
+              (Compilation of names in Indian languages & Flora Documentation)
+              Dr. Nidhan Singh (Western Himalayas) Dr. Manoj Chandran (Poaceae)
+              Dr. D. S. Rawat (Western Himalayas […]{" "}
+              <div className={style.more}>View More </div>
+            </div>
+          </div>
+          <div className={style.aboutImg}>
+            <img src={aboutus1} alt="img"></img>
+          </div>
+        </div>
+        <div className={style.parentRight}>
+          <div className={style.aboutImg}>
+            <img src={aboutus2} alt="img"></img>
+          </div>
+          <div className={style.contentRight}>
+            <div className={style.head}>2. Subject/Area Experts</div>
+            <div className={style.mainContent}>
+              Subject/ Area Experts: . Dr. Manoj Chandran (Poaceae) Dr. M. Sabu
+              (Zingiberaceae, Marantaceae & Musaceae) Dr. N.P. Balakishnan
+              (Euphorbiaceae) Dr. Chris Fraser-Jenkins (Pteridophytes) Dr. V P
+              Prasad (Cyperaceae) Dr. Mayur Nandikar (Commelinaceae) Dr.
+              Wojciech Adamowski (Impatiens) Dr. Anil Kumar (Poaceae) Dr. V.
+              Sampath Kumar (Lamiaceae) Dr. Pascal Bruggeman (Arisaema) Dr.
+              Andrew Henderson (Arecaceae) Dr. J […]
+              <div className={style.more}>View More</div>
+            </div>
+          </div>
+        </div>
+
+        {/* left */}
+        <div className={style.parentLeft}>
+          <div className={style.contentLeft}>
+            <div className={style.head}>3. The Pillars</div>
+            <div className={style.mainContent}>
+              The Pillars:. Name (Main area of expertise on Flora of India) Sh.
+              J.M. Garg (efloraofindia website) Sh. Saroj Kumar Kasaju (Flora
+              Documentation of Nepal) Dr. Gurcharan Singh (Western Himalayas) Dr
+              Pankaj Kumar (Orchidaceae) Sh. Dinesh Valke (Compilation of names
+              in Indian languages & Flora Documentation) Dr. Nidhan Singh
+              (Western Himalayas) Dr. Balkar Arya (Western Himalayas) Sh.
+              Surajit Koley (Flora Documentation of West […]
+              <div className={style.more}>View More </div>
+            </div>
+          </div>
+          <div className={style.aboutImg}>
+            <img src={aboutus1} alt="img"></img>
+          </div>
+        </div>
+
+        {/* right */}
+        <div className={style.parentRight}>
+          <div className={style.aboutImg}>
+            <img src={aboutus2} alt="img"></img>
+          </div>
+          <div className={style.contentRight}>
+            <div className={style.head}>4. Moderators</div>
+            <div className={style.mainContent}>
+              ‘Moderators’ are persons who have contributed substantially to
+              efloraofindia as a whole (after The Pillars & Subject Experts).
+              They have accepted the invitation to become a moderator to share
+              high responsibility in this regard. More such names will be added
+              when they come up to their level provided their behavior is up to
+              the mark at […]
+              <div className={style.more}>View More</div>
+            </div>
+          </div>
+        </div>
+
+        {/* 5 left */}
+
+        <div className={style.parentLeft}>
+          <div className={style.contentLeft}>
+            <div className={style.head}>5. Major Contributers</div>
+            <div className={style.mainContent}>
+              Abdulla, Noorafsa Abraham, Farida Adamowski, Wojciech Bronze
+              member Expert (Impatiens) Adavanne, Shivaprakash Adhikari,
+              Bhupendra Singh Expert (Rhododendrons) Adsul, Avinash Agrawala,
+              Dinesh Kumar Expert (Orchidaceae) Alfred, H. S. Bronze member
+              Expert Ambasta, Nirbhay Ambedkar, Prashant Bronze member
+              Ampalakkad, Ajit Ananth, Raghu Bronze member Anjaneyulu, Paradesi
+              Silver member Archer, Robert H. Expert (Celastraceae) Arun, N.
+              Bronze member Arunachalam, Raman Silver member Arya, Balkar
+              Diamond member […]<div className={style.more}>View More </div>
+            </div>
+          </div>
+          <div className={style.aboutImg}>
+            <img src={aboutus1} alt="img"></img>
+          </div>
+        </div>
+
+        {/* 6 right */}
+
+        <div className={style.parentRight}>
+          <div className={style.aboutImg}>
+            <img src={aboutus2} alt="img"></img>
+          </div>
+          <div className={style.contentRight}>
+            <div className={style.head}>
+              6. Details of some important contributors
+            </div>
+            <div className={style.mainContent}>
+              Details of some important contributors: . A. Lalithamba Avadhanam
+              Lalithamba – Star Expert of Oct.’17 Our tribute to two great
+              ladies on this group Thanks to Krishnaraj ji, Shantanu ji,
+              Lalithamba ji, Kunhikannan ji, Vijayasankar ji, D S Rawat ji,
+              Samir ji, Shrikant ji among others- Fabaceae (Faboideae) Fortnight
+              Oct’ 2015 Spl. thanks to Surajit […]
+              <div className={style.more}>View More</div>
+            </div>
+          </div>
+        </div>
+
+        {/* 7 left */}
+
+        <div className={style.parentLeft}>
+          <div className={style.contentLeft}>
+            <div className={style.head}>Top 10 Contriuters over the year</div>
+            <div className={style.mainContent}>
+              Top 10 posters on 1.1.23 List of top 10 posters as on 1.1.23 is
+              given below: Top Posters as on 1-1-2023 Total 1. J M Garg 99,270
+              2. Saroj Kasaju 39,097 3. Gurcharan Singh 36,471 4. Aarti Khale
+              14,123 5. Dinesh Valke 13,282 6. Usha Desai 12,000 7. Nidhan Singh
+              10,613 8. Balkar Singh 8,572 […]
+              <div className={style.more}>View More </div>
+            </div>
+          </div>
+          <div className={style.aboutImg}>
+            <img src={aboutus1} alt="img"></img>
+          </div>
+        </div>
+
+        {/*  Our Team */}
+
+        {/* <div className={style.ourTeam}>
+          <div className={style.top}>
+            <div className={style.heading2}>Our Team</div>
+            <div className={style.quote}>
+              We are a team dedicated to plants, with a passion and commitment.
+            </div>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+}
