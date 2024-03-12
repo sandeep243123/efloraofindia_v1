e902c1a1cde6e52adae2bfc1d85d01b11edbdc0d
HARIKESH KUSHWAHA <75518548+HarikeshKushwaha18@users.noreply.github.com>
2024-03-12 23:04:58 +0530

    Update Comments.js

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,54 +1,60 @@
 import {useState} from 'react';
 import Comment from "./Comment"
-import style from './Comment.module.css';
+// import style from './Comment.module.css';
 import useNode from '../hooks/useNode';
 
 const commentList = {
     id:1,
     items: [
-        // {
-        //     id: 123,
-        //     name: "Harikesh",
-        //     items:[{
-        //         id:234,
-        //         name: "Sandeep",
-        //         items:[{
-        //             id:345,
-        //             name:"Anjali",
-        //             items: []
-        //         }]
-        //     }]
-        // },
-        // {
-        //     id: 1234,
-        //     name: "Sandeep",
-        //     items:[{
-        //         id:2345,
-        //         name: "Harikesh",
-        //         items:[{
-        //             id:3456,
-        //             name:"Anjali",
-        //             items: []
-        //         }]
-        //     }] 
-        // }
+        {
+            id: 123,
+            text: "Harikesh is this side.!!",
+            name: "Harikesh",
+            items:[{
+                id:234,
+                text: "Sandeep is this side.!!",
+                name: "Sandeep",
+                items:[{
+                    id:345,
+                    text: "AnjaliRana is this side.!!",
+                    name:"Anjali",
+                    items: []
+                }]
+            }]
+        },
+        {
+            id: 1234,
+            text: "Sandeep is this side.!!",
+            name: "Sandeep",
+            items:[{
+                id:2345,
+                text: "Harikesh is this side.!!",
+                name: "Harikesh",
+                items:[{
+                    id:3456,
+                    text: "AnjaliRana is this side.!!",
+                    name:"Anjali",
+                    items: []
+                }]
+            }] 
+        }
     ]
 }
 
 function Comments(){
     const [commentsData, setCommentsData] = useState(commentList);
 
-    const { insertNode, editNode, deleteNode } = useNode();
+    const { insertNode, deleteNode } = useNode();
   
     const handleInsertNode = (folderId, item) => {
       const finalStructure = insertNode(commentsData, folderId, item);
       setCommentsData(finalStructure);
     };
   
-    const handleEditNode = (folderId, value) => {
-      const finalStructure = editNode(commentsData, folderId, value);
-      setCommentsData(finalStructure);
-    };
+    // const handleEditNode = (folderId, value) => {
+    //   const finalStructure = editNode(commentsData, folderId, value);
+    //   setCommentsData(finalStructure);
+    // };
   
     const handleDeleteNode = (folderId) => {
       const finalStructure = deleteNode(commentsData, folderId);
@@ -61,7 +67,7 @@ function Comments(){
         <div className="App">
             <Comment
                 handleInsertNode={handleInsertNode}
-                handleEditNode={handleEditNode}
+                // handleEditNode={handleEditNode}
                 handleDeleteNode={handleDeleteNode}
                 comment={commentsData}
             />
