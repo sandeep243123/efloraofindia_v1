de8ddd6769da632dc9b0c206154b64304d44613e
HARIKESH KUSHWAHA <75518548+HarikeshKushwaha18@users.noreply.github.com>
2024-03-12 23:01:25 +0530

    Update useNode.js

--- src/components/hooks/useNode.js
+++ src/components/hooks/useNode.js
@@ -1,9 +1,10 @@
 const useNode = () => {
-    const insertNode = function (tree, commentId, item) {
+    const insertNode = function (tree, commentId, item,uname) {
       if (tree.id === commentId) {
         tree.items.push({
           id: new Date().getTime(),
-          name: item,
+          text: item,
+          name: uname,
           items: [],
         });
   
@@ -12,24 +13,24 @@ const useNode = () => {
   
       let latestNode = [];
       latestNode = tree.items.map((ob) => {
-        return insertNode(ob, commentId, item);
+        return insertNode(ob, commentId, item, uname);
       });
   
       return { ...tree, items: latestNode };
     };
   
-    const editNode = (tree, commentId, value) => {
-      if (tree.id === commentId) {
-        tree.name = value;
-        return tree;
-      }
+    // const editNode = (tree, commentId, value) => {
+    //   if (tree.id === commentId) {
+    //     tree.name = value;
+    //     return tree;
+    //   }
   
-      tree.items.map((ob) => {
-        return editNode(ob, commentId, value);
-      });
+    //   tree.items.map((ob) => {
+    //     return editNode(ob, commentId, value);
+    //   });
   
-      return { ...tree };
-    };
+    //   return { ...tree };
+    // };
   
     const deleteNode = (tree, id) => {
       for (let i = 0; i < tree.items.length; i++) {
@@ -44,7 +45,7 @@ const useNode = () => {
       return tree;
     };
   
-    return { insertNode, editNode, deleteNode };
+    return { insertNode, deleteNode };
   };
   
   export default useNode;
