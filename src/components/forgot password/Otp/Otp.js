e692683f9d10608e2d3dcda2e1c993b488c4e667
chikki1224 <chikki.worspace@gmail.com>
2024-08-23 08:55:13 +0530

    changes in otp and image upload limit

--- src/components/forgot password/Otp/Otp.js
+++ src/components/forgot password/Otp/Otp.js
@@ -1,4 +1,4 @@
-import React, {useRef,useState } from 'react'
+/*import React, {useRef,useState } from 'react'
 import styles from './Otp.module.css'
 import { ToastContainer, toast } from 'react-toastify'
 import { useQuery, gql, useMutation } from "@apollo/client";
@@ -124,5 +124,162 @@ const [resetpasswordfunction] =
             <ToastContainer containerId="Warning"/>
         </div>
     )}
+
+export default Otp;
+*/
+import React, { useRef, useState, useEffect } from 'react';
+import styles from './Otp.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import { useQuery, gql, useMutation } from "@apollo/client";
+import 'react-toastify/dist/ReactToastify.css';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+function Otp() {
+    const inputRef = useRef();
+    const location = useLocation();
+    const { email } = location.state || {};
+    const navigate = useNavigate();
+    const [otpSent, setOtpSent] = useState(false);
+    const [resendTimer, setResendTimer] = useState(60);
+    const [otp, setOTP] = useState('');
+    const [inputData, setInputData] = useState({});
+
+    const notifyWarning = (msg) => {
+        toast.warning(` ${msg}!`, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            containerId: 'Warning'
+        });
+    };
+
+    const SEND_OTP_QUERY = gql`
+        query Query($details: sendOTPDetail!) {
+            sendOTP(details: $details)
+        }
+    `;
+
+    const RESET_PASSWORD_MUTATION = gql`
+        mutation ForgetPassword($details: forgetPasswordDetail!) {
+            forgetPassword(details: $details)
+        }
+    `;
+
+    const [resetPasswordFunction] = useMutation(RESET_PASSWORD_MUTATION, {
+        onCompleted: (data) => {
+            navigate('/login');
+        },
+        onError: (error) => {
+            notifyWarning(error.message);
+        }
+    });
+
+    const handleInputData = (e) => {
+        setInputData({
+            ...inputData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const validatePass = () => {
+        if (inputData.pass === '' || inputData.confirmPass === '') {
+            notifyWarning("Please enter your new password");
+        } else if (inputData.confirmPass === inputData.pass) {
+            resetPasswordFunction({
+                variables: {
+                    details: {
+                        "emailID": email,
+                        "newPassword": inputData.pass,
+                        "otp": parseInt(otp)
+                    }
+                }
+            });
+        } else {
+            notifyWarning("Passwords do not match");
+        }
+    };
+
+    const resendOtp = () => {
+        setOtpSent(false);
+        setResendTimer(60);
+
+        // Resend the OTP by re-triggering the query
+        refetch();
+    };
+
+    useEffect(() => {
+        if (otpSent && resendTimer > 0) {
+            const timer = setTimeout(() => {
+                setResendTimer(resendTimer - 1);
+            }, 1000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [resendTimer, otpSent]);
+
+    const { refetch } = useQuery(SEND_OTP_QUERY, {
+        variables: {
+            details: {
+                "emailID": email,
+                "isNew": false
+            }
+        },
+        onCompleted: () => {
+            console.log("OTP successfully sent");
+            setOtpSent(true);
+            setResendTimer(60); // Reset timer when OTP is sent
+        },
+        onError: (error) => {
+            console.error('Error:', error.message);
+            notifyWarning(error.message);
+        },
+        skip: !email // skip the query if email is not provided
+    });
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.contentContainer}>
+                <h1>Reset Password</h1>
+                <p>Enter the OTP and reset your password</p>
+                <div className={styles.ifield}>
+                    <input type="text" id="otp" placeholder="OTP" value={otp}
+                        onChange={(e) => {
+                            const input = e.target.value;
+                            if (/^\d{0,4}$/.test(input)) {
+                                setOTP(input);
+                            }
+                        }}
+                        maxLength={4}
+                        pattern="\d*"
+                        required
+                    />
+
+                    <input name='pass' type="password" placeholder='Set New password' onChange={handleInputData} />
+
+                    <input name='confirmPass' type="password" placeholder='Confirm password' onChange={handleInputData} />
+
+                    <div className={styles.resetBtn} onClick={validatePass}>Reset Password</div>
+
+                    {otpSent && resendTimer === 0 && (
+                        <div className={styles.resendBtn} onClick={resendOtp}>Resend OTP</div>
+                    )}
+
+                    {otpSent && resendTimer > 0 && (
+                        <div className={styles.resendTimer}>
+                            Resend OTP in {resendTimer}s
+                        </div>
+                    )}
+                </div>
+            </div>
+            <ToastContainer containerId="Error" />
+            <ToastContainer containerId="Warning" />
+        </div>
+    );
+}
 
 export default Otp;
--- src/components/signup/OtpForm.js
+++ src/components/signup/OtpForm.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useRef, useState, useContext } from 'react'
-import styles from './otpForm.module.css'
+import React, { useEffect, useRef, useState, useContext } from 'react';
+import styles from './otpForm.module.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Link, useLocation } from 'react-router-dom';
-import { gql, useMutation, useQuery } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
 import { AuthContext } from '../../services/AuthContext.js';
 import { useNavigate } from 'react-router-dom';
 import OtpInput from 'react-otp-input';
 
 function Otp() {
-    const inputRef = useRef()
+    const inputRef = useRef();
     const navigate = useNavigate();
     const location = useLocation();
-    const props = location.state || {}
-    console.log(location.state)
+    const props = location.state || {};
+    console.log(location.state);
 
     const notifyError = (msg) => {
         toast.error(`${msg}`, {
@@ -27,7 +27,7 @@ function Otp() {
             theme: "dark",
             containerId: 'Error'
         });
-    }
+    };
     const notifyWarning = (msg) => {
         toast.warning(` ${msg}!`, {
             position: "top-right",
@@ -40,7 +40,7 @@ function Otp() {
             theme: "light",
             containerId: 'Warning'
         });
-    }
+    };
     const notifySuccess = (msg) => {
         toast.success(` ${msg}!`, {
             position: "top-center",
@@ -53,112 +53,95 @@ function Otp() {
             theme: "light",
             containerId: 'Success'
         });
-    }
+    };
 
     const [loggedIn, setLoggedIn] = useState(false);
     const { isLoggedIn, login, setUser } = useContext(AuthContext);
     const [Otp, setOtp] = useState('');
+    const [timer, setTimer] = useState(60); // Timer state for countdown
+    const [canResend, setCanResend] = useState(false); // State to control resend button visibility
 
     const signupMutation = gql`
         mutation Signup($details: signupDetails!) {
             signup(details: $details) {
-            token
-            currentRating
-            name
-            email
-            accountType
+                token
+                currentRating
+                name
+                email
+                accountType
             }
         }
-    `
+    `;
 
-    const [signupfunc] =
-        useMutation(signupMutation, {
-            onCompleted: (data) => {
-                if (data.signup && data.signup.token) {
-                    
-                    const { token, ...userData } = data.signup;
-                    localStorage.setItem('authToken', token);
-                    setUser(userData);
-                    setLoggedIn(true);
-                    login();
-
-                }
-                else {
-                    console.error('Error signing up: No token received.');
-                }
-            },
-            onError: (error) => {
-                console.error('Error:', error.message);
-                notifyError(error.message);
+    const [signupfunc] = useMutation(signupMutation, {
+        onCompleted: (data) => {
+            if (data.signup && data.signup.token) {
+                const { token, ...userData } = data.signup;
+                localStorage.setItem('authToken', token);
+                setUser(userData);
+                setLoggedIn(true);
+                login();
+            } else {
+                console.error('Error signing up: No token received.');
             }
-        })
+        },
+        onError: (error) => {
+            console.error('Error:', error.message);
+            notifyError(error.message);
+        }
+    });
 
     useEffect(() => {
         if (loggedIn) {
-            notifySuccess("You are successfully registered")
+            notifySuccess("You are successfully registered");
             setTimeout(() => {
-                navigate("/")
-            }, 1000)
-
+                navigate("/");
+            }, 1000);
         }
     }, [loggedIn, navigate]);
 
-
     useEffect(() => {
         notifySuccess('OTP sent Successfully');
-    }, [])
+    }, []);
+
     useEffect(() => {
-        const inputs = inputRef.current
-        inputs.addEventListener("input", function (e) {
-            const target = e.target;
-            const val = target.value;
-            if (isNaN(val)) {
-                target.value = "";
-                return;
-            }
-
-            if (val != "") {
-                const next = target.nextElementSibling;
-                if (next) {
-                    next.focus();
+        const countdown = setInterval(() => {
+            setTimer((prevTimer) => {
+                if (prevTimer === 1) {
+                    clearInterval(countdown);
+                    setCanResend(true);
+                    return 0;
+                } else {
+                    return prevTimer - 1;
                 }
-            }
-        });
-        inputs.addEventListener("keyup", function (e) {
-            const target = e.target;
-            const key = e.key.toLowerCase();
+            });
+        }, 1000);
 
-            if (key == "backspace" || key == "delete") {
-                target.value = "";
-                const prev = target.previousElementSibling;
-                if (prev) {
-                    prev.focus();
-                }
-                return;
-            }
-        });
-    })
+        return () => clearInterval(countdown);
+    }, []);
 
-    const [inputotp, setInputotp] = useState(0);
+    const handleResendOtp = () => {
+        setCanResend(false);
+        setTimer(60);
+        notifySuccess('OTP resent successfully');
+        // Logic to resend OTP can be added here
+    };
 
     const getotp = () => {
         const otp = parseInt(Otp);
 
         console.log(otp);
-        setInputotp(otp);
-        signupfunc({ variables: { details: { "email": props.email, "name": props.name, "password": props.password,"otp":otp } } })
-        
+        signupfunc({ variables: { details: { "email": props.email, "name": props.name, "password": props.password, "otp": otp } } });
     };
 
     const otpRef = useRef(null);
 
     useEffect(() => {
-      // Autofocus on the first input when the component mounts
-      if (otpRef.current) {
-        otpRef.current.focus();
-      }
+        // Autofocus on the first input when the component mounts
+        if (otpRef.current) {
+            otpRef.current.focus();
+        }
     }, []);
-
 
     return (
         <div className={styles.container}>
@@ -167,45 +150,50 @@ function Otp() {
                 <p>OTP sent to {props.email}</p>
 
                 <div className={styles.ifield}>
-                    <div ref={inputRef} id="inputs" class={styles.inputs}>
-                    <OtpInput
-                                value={Otp}
-                                onChange={setOtp}
-                                numInputs={4}
-                                ref={otpRef}
-                                inputStyle={{
-                                    color: 'black',
-                                    width: "30px",
-                                    marginBottom: "10px",
-                                    height: "30px",
-                                    borderTop: "none",
-                                    borderLeft: "none",
-                                    borderRight: "none",
+                    <div ref={inputRef} id="inputs" className={styles.inputs}>
+                        <OtpInput
+                            value={Otp}
+                            onChange={setOtp}
+                            numInputs={4}
+                            ref={otpRef}
+                            inputStyle={{
+                                color: 'black',
+                                width: "30px",
+                                marginBottom: "10px",
+                                height: "30px",
+                                borderTop: "none",
+                                borderLeft: "none",
+                                borderRight: "none",
+                            }}
+                            renderSeparator={<span
+                                style={{
+                                    fontSize: "7px",
+                                    marginLeft: "5px",
+                                    marginRight: "5px",
                                 }}
-                                renderSeparator={<span
-                                    style={{
-                                      fontSize: "7px",
-                                      marginLeft: "5px",
-                                      marginRight: "5px",
-                                    }}
-                                  >
-                                    {" "}
-                                  </span>}
-                                renderInput={(props) => <input {...props} />}
-                            />
+                            >
+                                {" "}
+                            </span>}
+                            renderInput={(props) => <input {...props} />}
+                        />
                     </div>
 
                     {/* here connect to landing page */}
-                    <p className={styles.verifyBtn} onClick={() => {
-                        getotp();
-                    }}>Verify</p>
+                    <p className={styles.verifyBtn} onClick={getotp}>Verify</p>
+
+                    {/* Resend OTP Button */}
+                    {canResend ? (
+                        <p className={styles.resendBtn} onClick={handleResendOtp}>Resend OTP</p>
+                    ) : (
+                        <p>Resend OTP in {timer}s</p>
+                    )}
                 </div>
             </div>
             <ToastContainer containerId="Error" />
             <ToastContainer containerId="Warning" />
             <ToastContainer containerId="Success" />
         </div>
-    )
+    );
 }
 
-export default Otp
+export default Otp;
